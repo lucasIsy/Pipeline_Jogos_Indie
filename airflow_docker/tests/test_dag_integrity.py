ce7eab1791e5982e18136c3477e4eb92c0bb6186
Lucas isy <isylucywyn@outlook.com>
2026-03-10 10:00:38 -0300

    Delete airflow_docker directory

--- airflow_docker/tests/test_dag_integrity.py
+++
@@ -1,8 +0,0 @@
-from airflow.models import DagBag
-
-def test_dag_integrity():
-    # O DagBag procura arquivos .py na pasta especificada
-    dag_bag = DagBag(dag_folder='airflow/dags/', include_examples=False)
-    
-    # O teste falha se houver qualquer erro de importação
-    assert len(dag_bag.import_errors) == 0, f"Erros encontrados: {dag_bag.import_errors}"
